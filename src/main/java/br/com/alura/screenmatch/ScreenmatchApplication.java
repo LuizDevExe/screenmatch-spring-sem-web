90fd5170887fa8c4eb6d716f0f61dcba59ee51c1
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-08-14 16:14:08 -0300

    Filtrando episódios por data

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,9 @@
 package br.com.alura.screenmatch;
 
-import Model.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import services.ConsumoApi;
-import services.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -14,15 +12,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
 
-        var ConsumoApi = new ConsumoApi();
-        var json = ConsumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9bc310b6");
-        System.out.println(json);
 
-        ConverteDados converteDados = new ConverteDados();
-        DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
-        System.out.println(dadosSerie);
-    }
 }
--- src/main/java/services/IConverteDados.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public interface IConverteDados {
-    <T> T obterDados(String json, Class<T> classe);
-}
